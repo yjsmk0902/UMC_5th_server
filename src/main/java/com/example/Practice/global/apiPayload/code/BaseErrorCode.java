3d5d2a5f3c1e7285fb90d7581b54514a6848d5e0
Luke_YS <yjsmk0902@gmail.com>
2023-12-26 21:16:47 +0900

    Feat: API Response 프레임 추가

---
+++ src/main/java/com/example/Practice/global/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,7 @@
+package com.example.Practice.global.apiPayload.code;
+
+public interface BaseErrorCode {
+
+    public ErrorReasonDTO getReason();
+    public ErrorReasonDTO getReasonHttpStatus();
+}
